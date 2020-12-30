696ccb5abd5d71c22da6e3b298882ccfc105d129
Oliver Borchert <oliver@taskfleet.io>
2020-12-30 22:06:57 +0100

    Fix tls auth generation

--- pkg/crypto/dh.go
+++ pkg/crypto/dh.go
@@ -2,28 +2,16 @@ package crypto
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
 	"os/exec"
 )
 
 // GenerateDhParams generates Diffie-Hellman parameters of the given size and returns the generated
 // parameters upon success. This method may take multiple minutes to run.
 func GenerateDhParams(bits int) ([]byte, error) {
-	file, err := ioutil.TempFile("", "dh-params-*")
+	cmd := exec.Command("openssl", "dhparam", "-out", "/dev/stdout", fmt.Sprintf("%d", bits))
+	out, err := cmd.Output()
 	if err != nil {
-		return nil, fmt.Errorf("failed to create temporary file: %s", err)
-	}
-	defer os.Remove(file.Name())
-
-	cmd := exec.Command("openssl", "dhparam", "-out", file.Name(), fmt.Sprintf("%d", bits))
-	if err := cmd.Run(); err != nil {
 		return nil, fmt.Errorf("failed generating dh params: %s", err)
 	}
-
-	contents, err := ioutil.ReadFile(file.Name())
-	if err != nil {
-		return nil, fmt.Errorf("failed to read generated dh params: %s", err)
-	}
-	return contents, nil
+	return out, nil
 }
--- pkg/crypto/tls.go
+++ pkg/crypto/tls.go
@@ -7,7 +7,7 @@ import (
 
 // GenerateTLSAuth generates an OpenVPN static key to be used.
 func GenerateTLSAuth() ([]byte, error) {
-	cmd := exec.Command("openvpn", "--genkey")
+	cmd := exec.Command("openvpn", "--genkey", "--secret", "/dev/stdout")
 	out, err := cmd.Output()
 	if err != nil {
 		return nil, fmt.Errorf("failed generating tls auth: %s", err)
